4a8bd5cbdee41023995a43d6e8fd98e02a086074
amirrezanosrati <103998422+amirrezanosrati@users.noreply.github.com>
2025-09-19 14:24:28 +0330

    Update app.py

--- app.py
+++ app.py
@@ -55,6 +55,7 @@ def send_message(chat_id, text):
     except Exception as e:
         logging.error(f"Error sending message: {e}")
 
+# این خط برای Render ضروری است
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
